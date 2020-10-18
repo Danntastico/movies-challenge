5c01e8083e5bd3b3432686d5c778b65fc2272bfc
daniloWindows <nicolasdpf@gmail.com>
2020-10-17 22:44:43 -0500

    Setting typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react'
+
+export const App: FunctionComponent = () => {
+    return (
+        <div>
+            <h1> Hola mundo</h1>
+        </div>
+    )
+}
