d8158df394c44f3203a09f0c6519b08debc5cb02
daniloWindows <nicolasdpf@gmail.com>
2020-10-17 23:00:01 -0500

    Unit testin to button

---
+++ src/tests/components/button/button.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import {Button} from '../../../components/button/Button';
+
+import '@testing-library/jest-dom';
+
+describe('Unit test on <Button />', () => {
+    const onCLickButtonHandler = jest.fn();
+    let wrapper = mount(<Button onClick={onCLickButtonHandler} textLabel="Test Btn" />)
+
+    test('should Render correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+    test('Simulate onClick', () => {
+        wrapper.find('button').simulate('click')
+        expect(onCLickButtonHandler.mock.calls.length).toEqual(1);
+    });
+})
