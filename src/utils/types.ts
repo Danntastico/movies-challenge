b128ea960bb2c5f3ce1315990c7611a119286ecd
daniloWindows <nicolasdpf@gmail.com>
2020-10-18 13:44:38 -0500

    More interfaces for data mapping to TMDB api

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -13,3 +13,27 @@ export interface TMDBMovieImages {
     id: number;
     posters: Array<TMDBImageInformation>;
 }
+
+export interface TMDBMovieInformation {
+    popularity: number;
+    vote_count: number;
+    video: boolean;
+    poster_path: string;
+    id: number;
+    adult: boolean;
+    backdrop_path: null | string;
+    original_language: string;
+    original_title: string;
+    genre_ids: Array<number>;
+    title: string;
+    vote_average: number;
+    overview: string;
+    release_date: string;
+}
+
+export interface TMDBSearchMovieResponse {
+    page: number,
+    total_results: number;
+    total_pages: number;
+    results: Array<TMDBMovieInformation>;
+}
