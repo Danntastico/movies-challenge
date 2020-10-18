0f99166fb770b25ce3793e361a18d90c9a68ff20
daniloWindows <nicolasdpf@gmail.com>
2020-10-17 23:28:29 -0500

    adding constants from access to TMDB

---
+++ src/constants/theMovieDatabaseAPI.ts
@@ -0,0 +1,3 @@
+export const API_KEY = "ebb579ef6204cfac9d15190e20c1eb53";
+export const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYmI1NzllZjYyMDRjZmFjOWQxNTE5MGUyMGMxZWI1MyIsInN1YiI6IjVlOGZlNDhjMDBmYjZiMDAxM2EwOGE4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HAv3_3o-4xC703vq_23xmi2XceWmrdBvOZ1_D25nwFA";
+export const MAIN_URL = "https://api.themoviedb.org/3/movie/76341?api_key=";
