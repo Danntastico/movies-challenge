d69086f00465e529edaa7a92e6e80ee8bd046595
Juan Felipe <u1201610@unimilitar.edu.co>
2020-10-18 21:56:37 -0500

    Background color changed and globalStyles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import { Navbar } from 'components/navbar/Navbar';
+import { GlobalStyle } from './styles/GlobalStyles';
+
 export const App: FunctionComponent = () => {
     return (
         <>
+            <GlobalStyle/>
             <Navbar />
         </>
     )
