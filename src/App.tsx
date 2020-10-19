430a2985b14050dbacc147d126a973d30cfb594e
Javier Garcia <javier.garcia9611@gmail.com>
2020-10-19 00:10:02 -0500

    Se importa el componente Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { FunctionComponent } from 'react'
 import { Navbar } from 'components/navbar/Navbar';
+import { Cards } from 'components/card/Card';
 import { GlobalStyle } from './styles/GlobalStyles';
 
 export const App: FunctionComponent = () => {
     return (
         <>
-            <GlobalStyle/>
+            <GlobalStyle />
             <Navbar />
+            <Cards />
         </>
     )
 }
