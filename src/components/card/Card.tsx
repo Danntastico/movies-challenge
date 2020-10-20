f45754ff5cad7dfc3dfaba8630241e5e8773b2cc
Javier Garcia <javier.garcia9611@gmail.com>
2020-10-19 22:33:52 -0500

    Se agrega el componente CardRating y CardParagraph junto con otras 3 tarjetas

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { RowStyled, ColStyled, CardStyled, CardBody, CardTitle, CardText, CardFooter } from './CardStyles';
+import { RowStyled, ColStyled, CardStyled, CardBody, CardTitle, CardRating, CardText, CardFooter, CardParagraph } from './CardStyles';
 
 export const Cards: FunctionComponent = () => {
     return (
@@ -10,8 +10,53 @@ export const Cards: FunctionComponent = () => {
                         <CardStyled>    
                             <CardBody >
                                 <CardTitle> 1 </CardTitle>
-                                <CardText> Calificación </CardText>
-                                <CardFooter>2 days ago
+                                <CardRating> 6.7 </CardRating>
+                                <CardText> Las crónicas de narnia: la travesía del viajero del alba </CardText>
+                                <CardFooter>                                    
+                                    <CardParagraph> Comedy </CardParagraph>
+                                    <CardParagraph> Fantasy </CardParagraph>
+                                </CardFooter> 
+                            </CardBody>
+                        </CardStyled>
+                    </ColStyled>
+
+                    <ColStyled sm={6} md={4} lg={3} className="align-items-stretch">
+                        <CardStyled>    
+                            <CardBody >
+                                <CardTitle> 1 </CardTitle>
+                                <CardRating> 6.7 </CardRating>
+                                <CardText> Las crónicas de narnia: la travesía del viajero del alba </CardText>
+                                <CardFooter>                                    
+                                    <CardParagraph> Comedy </CardParagraph>
+                                    <CardParagraph> Fantasy </CardParagraph>
+                                </CardFooter> 
+                            </CardBody>
+                        </CardStyled>
+                    </ColStyled>
+
+                    <ColStyled sm={6} md={4} lg={3} className="align-items-stretch">
+                        <CardStyled>    
+                            <CardBody >
+                                <CardTitle> 1 </CardTitle>
+                                <CardRating> 6.7 </CardRating>
+                                <CardText> Las crónicas de narnia: la travesía del viajero del alba </CardText>
+                                <CardFooter>                                    
+                                    <CardParagraph> Comedy </CardParagraph>
+                                    <CardParagraph> Fantasy </CardParagraph>
+                                </CardFooter> 
+                            </CardBody>
+                        </CardStyled>
+                    </ColStyled>
+
+                    <ColStyled sm={6} md={4} lg={3} className="align-items-stretch">
+                        <CardStyled>    
+                            <CardBody >
+                                <CardTitle> 1 </CardTitle>
+                                <CardRating> 6.7 </CardRating>
+                                <CardText> Las crónicas de narnia: la travesía del viajero del alba </CardText>
+                                <CardFooter>                                    
+                                    <CardParagraph> Comedy </CardParagraph>
+                                    <CardParagraph> Fantasy </CardParagraph>
                                 </CardFooter> 
                             </CardBody>
                         </CardStyled>
