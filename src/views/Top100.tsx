93d72581431fbe98732e55554914cbe9065d5ffa
Javier Garcia <javier.garcia9611@gmail.com>
2020-10-21 00:28:48 -0500

    Se quito el titulo h1

--- src/views/Top100.tsx
+++ src/views/Top100.tsx
@@ -5,7 +5,6 @@ import { MovieContainer } from 'components/movieContainer/MovieContainer';
 export const Top100 = () => {
     return (
         <>
-            <h1>Top100 section</h1>
             <MovieContainer>
                 <Card title="Test movie1" rating="5.9" keyValue="1" description="Carlos es el sastre más prestigioso de Granada. Un hombre respetable. Su vida es el trabajo y comer. Pero no cualquier cosa: se alimenta de mujeres desconocidas, con las que no tiene ningún vínculo emocional. Pero todo eso cambia el día que conoce a la joven rumana Nina, que busca desesperadamente a su hermana gemela Alexandra, desaparecida días atrás. Carlos es el sastre más prestigioso de Granada. Un hombre respetable. Su vida es el trabajo y comer. Pero no cualquier cosa: se alimenta de mujeres desconocidas, con las que no tiene ningún vínculo emocional. Pero todo eso cambia el día que conoce a la joven rumana Nina, que busca desesperadamente a su hermana gemela Alexandra, desaparecida días atrás." />
                 <Card title="Test movie1" rating="5.9" keyValue="2" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown "/>
