f4c471eb843055537d400c9fca60184486f1e1d4
daniloWindows <nicolasdpf@gmail.com>
2020-10-20 19:20:29 -0500

    Añadiendo data quemada a top100

--- src/views/Top100.tsx
+++ src/views/Top100.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Card } from 'components/card/Card';
 
 export const Top100 = () => {
     return (
-        <div>
-            <h1>Top100</h1>
-        </div>
+        <>
+            <h1>Top100 section</h1>
+            <div className="movieContainer">
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+                <Card title="Test movie1" rating="5.9" keyValue="1.0" />
+            </div>
+        </>
     )
 }
