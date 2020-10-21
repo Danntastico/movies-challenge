26eb1f56c763ee7bc102cfacbb5eb68ea497202d
daniloWindows <nicolasdpf@gmail.com>
2020-10-20 19:20:36 -0500

    Añadiendo approuter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { FunctionComponent } from 'react'
 import { Navbar } from 'components/navbar/Navbar';
-import { Cards } from 'components/card/Card';
 import { GlobalStyle } from './styles/GlobalStyles';
+import { AppRouter } from 'router/AppRouter';
 
 export const App: FunctionComponent = () => {
     return (
         <>
             <GlobalStyle />
             <Navbar />
-            <Cards />
+            <AppRouter />
         </>
     )
 }
