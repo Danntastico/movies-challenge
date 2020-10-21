e398e235b7ff82555c1079123131bea71dc657e8
daniloWindows <nicolasdpf@gmail.com>
2020-10-20 19:07:59 -0500

    Se realiza test al componente CARD

---
+++ src/tests/components/Card/Card.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Card } from 'components/card/Card';
+import { mount } from 'enzyme';
+
+import '@testing-library/jest-dom'
+describe('Unit Testing to CARD component', () => {
+    const shall = mount(<Card title="testTitle" rating="1" keyValue="1" />)
+
+    test('Should render correctly', () => {
+        expect(shall).toMatchSnapshot();    
+    });
+    test('Should show title correctly', () => {
+        const paragraphs = shall.find('p');
+        expect(paragraphs.at(1).text().trim()).toBe('testTitle');
+    });
+});
