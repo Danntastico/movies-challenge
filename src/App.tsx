a24ab2f1ce17e22fdbcc44a22e190c4383dfcc2e
daniloWindows <nicolasdpf@gmail.com>
2020-10-21 19:53:07 -0500

    blbal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles';
 import { AppRouter } from 'router/AppRouter';
 
 export const App: FunctionComponent = () => {
-    
-    
-
     return (
         <>
             <GlobalStyle />
