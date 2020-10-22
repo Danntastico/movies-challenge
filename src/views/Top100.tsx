fad70104653d437fbd2bf0b5fba126cc267b2b08
daniloWindows <nicolasdpf@gmail.com>
2020-10-22 02:06:23 -0500

    Conflictos

--- src/views/Top100.tsx
+++ src/views/Top100.tsx
@@ -55,19 +55,6 @@ export const Top100 = () => {
                         )}
                 </MovieContainer>
             </InfiniteScroll>
-            <MovieContainer>
-                <Card title="Test movie1" rating="5.9" keyValue="1" description="Carlos es el sastre más prestigioso de Granada. Un hombre respetable. Su vida es el trabajo y comer. Pero no cualquier cosa: se alimenta de mujeres desconocidas, con las que no tiene ningún vínculo emocional. Pero todo eso cambia el día que conoce a la joven rumana Nina, que busca desesperadamente a su hermana gemela Alexandra, desaparecida días atrás. Carlos es el sastre más prestigioso de Granada. Un hombre respetable. Su vida es el trabajo y comer. Pero no cualquier cosa: se alimenta de mujeres desconocidas, con las que no tiene ningún vínculo emocional. Pero todo eso cambia el día que conoce a la joven rumana Nina, que busca desesperadamente a su hermana gemela Alexandra, desaparecida días atrás." />
-                <Card title="Test movie1" rating="5.9" keyValue="2" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown "/>
-                <Card title="Test movie1" rating="5.9" keyValue="3" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="4" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="5" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="6" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="7" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="8" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="9" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="10" description="Perro hpta"/>
-                <Card title="Test movie1" rating="5.9" keyValue="11" description="Perro hpta"/>
-            </MovieContainer>
             <TopButton></TopButton>
         </>
     )
