83e2ccc8d2205f6a4729def7e15cede1bf935102
daniloWindows <nicolasdpf@gmail.com>
2020-10-22 02:03:54 -0500

    Test

--- src/tests/components/Card/Card.test.tsx
+++ src/tests/components/Card/Card.test.tsx
@@ -4,7 +4,16 @@ import { mount } from 'enzyme';
 
 import '@testing-library/jest-dom'
 describe('Unit Testing to CARD component', () => {
-    const shall = mount(<Card title="testTitle" rating="1" keyValue="1" />)
+    const shall = 
+        mount(
+            <Card 
+                title="testTitle" 
+                rating={1} 
+                keyValue={1} 
+                tmdbID={1} 
+                > 
+                category 
+            </Card>)
 
     test('Should render correctly', () => {
         expect(shall).toMatchSnapshot();    
